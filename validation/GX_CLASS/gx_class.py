4b419c4ffe6e6a74981a0a55901710672fce5369
Thien <thethien1708@gmail.com>
2025-09-12 16:09:28 +0700

    nearly done GXClass

--- validation/GX_CLASS/gx_class.py
+++ validation/GX_CLASS/gx_class.py
@@ -14,14 +14,14 @@ class GXClass:
             os.getenv("POSTGRES_DB")
         )
 
-        self.context = gx.get_context(mode="file")
+        self._context = gx.get_context()
         self.datasource_name = datasource_name
        
     def create_datasource(self):
         try:
-            datasources = self.context.list_datasources()
+            datasources = self._context.list_datasources()
             if not any(d['name'] == self.datasource_name for d in datasources):
-                self.context.data_sources.add_postgres(
+                self._context.data_sources.add_postgres(
                     connection_string=self._connection_string,
                     name=self.datasource_name
                 )
@@ -31,18 +31,36 @@ class GXClass:
         except Exception as e:
             print(f"Error: {e}")
 
-    # def create_asset(self,table_name):
-    #     try:
-            
-    #         datasrc = self.context.data_sources.get(self.datasource_name)
-    #         datasrc.add_table_asset(
-    #             table_name=table_name,
-    #         )
-    #     except Exception as e:
-    #         print(f"{e}")
+    def create_asset(self,asset_name,table_name):
+        try:
+            datasrc = self._context.data_sources.get(self.datasource_name)
+            datasrc.add_table_asset(
+                name=asset_name,
+                table_name=table_name,
+            )
+            print(f"Created asset {asset_name}")
+        except Exception as e:
+            print(f"{e}")
 
-    # def 
+    def create_expectation_suite(self,expectation_suite_name):
+        try:
+            suite = gx.ExpectationSuite(name=expectation_suite_name)
+            suite = self._context.suites.add(suite)
+            print(f"Created expectation suite {expectation_suite_name}")
+        except Exception as e:
+            print(f"{e}")
+        
+    def add_expectation_to_suite(self,expectation_suite_name,expect_func_obj,col_effect):
+        try:
+            suite = self._context.suites.get(expectation_suite_name)
+            suite.add_expectation(expect_func_obj(column=col_effect))
+            print(f"Added expectation to suite {expectation_suite_name}")
+        except Exception as e:
+            print(f"{e}")
     
+    
+        
 if __name__ == "__main__":
     gx_obj = GXClass(datasource_name="job_database")
     gx_obj.create_datasource()
+    gx_obj.create_asset(asset_name="job_database",table_name="jobs")
