67b5d9f0ebdb5f15d2541fd1123856e1f4406c85
Thien <thethien1708@gmail.com>
2025-09-25 17:21:15 +0700

    succes connect to duckdb

---
+++ DuckDB/test/test_connect.py
@@ -0,0 +1,35 @@
+import duckdb
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+def show_schemas(con):
+    # 1) Liệt kê schemas (dùng information_schema thay SHOW SCHEMAS)
+    print("Schemas:")
+    print(con.execute("SELECT schema_name FROM information_schema.schemata;").fetchall())
+
+def show_tables(con, schema_name):
+    # 2) Liệt kê tables trong schema os.getenv("DUCKDB_STAGING_SCHEMA")
+    print(f"Tables in {os.getenv('DUCKDB_STAGING_SCHEMA')}:")
+    print(con.execute(f"""
+        SELECT table_name 
+        FROM information_schema.tables 
+        WHERE table_schema = '{schema_name}';
+    """).fetchall())
+
+def truncate_table(con, schema_name, table_name):
+    con.execute(f"TRUNCATE TABLE {schema_name}.{table_name}")
+
+def spacing():
+    print()
+    print("--------------------------------")
+    print()
+    
+def main():
+    con = duckdb.connect(os.getenv("DUCKDB_PATH"))
+    truncate_table(con, os.getenv("DUCKDB_STAGING_SCHEMA"), os.getenv("DUCKDB_STAGING_TABLE"))
+    con.close()
+    
+    
+if __name__ == "__main__":
+    main()
